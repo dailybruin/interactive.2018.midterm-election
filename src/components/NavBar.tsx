9a5e6d5b905995e0852937f43736acdcaa236fcd
Richard <richbluehill@gmail.com>
2018-11-04 23:06:03 -0800

    nav bar update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled, {css} from 'react-emotion'
 import Headroom from 'react-headroom'
+import HeaderImg from '../assets/header.png';
 
 const RedButton = styled('button')`
   background-color: #CF5F5F;
@@ -10,13 +11,11 @@ const RedButton = styled('button')`
   align-self: flex-end;
   margin-right: 50px;
 `
-
 const LandingText = styled('p')`
-  width: 600px;
+  max-width: 600px;
   margin: 0px;
   margin-right: 50px;
 `
-
 const TitleBackground = styled('div')`
   width: 100%;
   height: 100%;
@@ -26,24 +25,13 @@ const TitleBackground = styled('div')`
   align-items: center;
   height: 240px;
   z-index: 10;
-  @media (max-width: 500px) {
-    height: 45vw;
-  }
-`
-
-const YearBox = styled('div')`
-  width: 100px;
-  height: 140px;
-  background: #1C568C;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-left: 50px;
-  @media (max-width: 500px) {
-    margin-left: 5vw;
-    width: 20vw;
-    height: 27vw;
+  box-shadow: 0 5px 10px -10px #000000;
+  @media (max-width: 1100px) {
+    min-height: 250px;
+    height: 30vw;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 `
 const NavBarContent = styled('div')`
@@ -52,55 +40,21 @@ flex-direction: column;
 margin-left: 10px;
 height: 140px;
 justify-content: center;
-@media (max-width: 500px) {
-  height: 27vw;
+@media (max-width: 1100px) {
+  align-items: center;
+  margin-top: 5px;
 }
-`
-const YearText = styled('p')`
-  margin: 0;
-  font-style: normal;
-  font-weight: 900;
-  line-height: 50px;
-  font-size: 60px;
-  color: #FFFFFF;
-  @media (max-width: 500px) {
-    font-size: 12vw;
-    line-height: 12vw;
-  }
-`
-
-const Title = styled('p')`
-  margin: 0;
-  font-style: bold;
-  font-weight: 900;
-  line-height: 50px;
-  font-size: 60px;
-  @media (max-width: 500px) {
-    font-size: 12vw;
-    line-height: 12vw;
-  }
 `
 
 class NavBar extends React.Component<{},{}> {
 
   render() {
     return(
-      <Headroom>
+      <Headroom style={{zIndex: 2}}>
         <TitleBackground>
-          <div className={css`
-            display: flex;
-          `}>
           <NavBarContent>
-            <YearBox>
-              <YearText>20</YearText>
-              <YearText>18</YearText>
-            </YearBox>
+            <img style={{height: "100%", margin: 0}} src={HeaderImg}/>
           </NavBarContent>
-          <NavBarContent>
-            <Title>MIDTERM</Title>
-            <Title>ELECTIONS</Title>
-          </NavBarContent>
-          </div>
           <NavBarContent>
             <LandingText>
               THE DAILY BRUIN’S 2018 MIDTERM ELECTION GUIDE: Click on the illustration below to see read about local, state, and national races or view our list of endorsements.
